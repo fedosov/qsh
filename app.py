6937d482b972c31d7022057e40f8bd707bcd3d7e
Mikhail Fedosov <tbs.micle@gmail.com>
2012-04-19 02:02:59 +0400

    Sent small JPG image of desktop and received it. (TODO: break datagram size limitations of UDP)

--- app.py
+++ app.py
@@ -19,34 +19,51 @@ class ScreenViewWindow(QDialog):
 		self.imgPreview.setFixedSize(320, 240)
 		self.updateScreenshot()
 
+		buttonBox = QtGui.QDialogButtonBox()
 		self.btnPush = QPushButton(u"Отправка картинки")
 		self.btnPush.clicked.connect(self.pushScreen)
+		buttonBox.addButton(self.btnPush, QtGui.QDialogButtonBox.ActionRole)
 		self.btnPull = QPushButton(u"Загрузка картинки")
 		self.btnPull.clicked.connect(self.pullScreen)
+		buttonBox.addButton(self.btnPull, QtGui.QDialogButtonBox.ActionRole)
 
 		self.socket = QtNetwork.QUdpSocket(self)
 		self.socket.bind(7740)
 		self.socket.readyRead.connect(self.socketReadyRead)
-		#self.socket.writeDatagram("OKAY!", QtNetwork.QHostAddress.LocalHost, 7740)
-		self.socket.writeDatagram("OKAY!", QtNetwork.QHostAddress.Broadcast, 7740)
+
+		screenBA = QtCore.QByteArray()
+		screenBuf = QtCore.QBuffer(screenBA)
+		screenBuf.open(QtCore.QBuffer.WriteOnly)
+		self.screen.save(screenBuf, "JPG", 0)
+		print type(screenBuf.data()), len(screenBuf.data())
+		self.socket.writeDatagram(screenBuf.data(), QtNetwork.QHostAddress(QtNetwork.QHostAddress.Broadcast), 7740)
 
 		layout = QVBoxLayout()
 		layout.addWidget(self.imgPreview)
-		layout.addWidget(self.btnPush)
-		layout.addWidget(self.btnPull)
+		layout.addWidget(buttonBox)
 		self.setLayout(layout)
 
 	def socketReadyRead(self):
+		print "socketReadyRead"
 		while self.socket.hasPendingDatagrams():
+			print "IN!", self.socket.pendingDatagramSize()
 			(data, sender, senderPort) = self.socket.readDatagram(self.socket.pendingDatagramSize())
-			print sender, senderPort, data
+			print sender, senderPort, len(data)
+#			screenBA = QtCore.QByteArray()
+#			screenBuf = QtCore.QBuffer(screenBA)
+#			screenBuf.open(QtCore.QBuffer.WriteOnly)
+#			screenBuf.setData(data)
+			self.screen = QtGui.QPixmap()
+			self.screen.loadFromData(data, "JPG")
+			self.imgPreview.setPixmap(self.screen)
+
 
 	def updateScreenshot(self):
 		"""Снять и обновить скриншот своего компьютера"""
 		desktop_size = QtGui.QApplication.desktop().size()
-		pix = QtGui.QPixmap.grabWindow(QtGui.QApplication.desktop().winId())
-		pix = pix.copy(0, 0, desktop_size.width(), desktop_size.height()).scaledToWidth(320, QtCore.Qt.SmoothTransformation)
-		self.imgPreview.setPixmap(pix)
+		self.screen = QtGui.QPixmap.grabWindow(QtGui.QApplication.desktop().winId())
+		self.screen = self.screen.copy(0, 0, desktop_size.width(), desktop_size.height()).scaledToWidth(120, QtCore.Qt.SmoothTransformation)
+		self.imgPreview.setPixmap(self.screen)
 
 	def pullScreen(self):
 		"""Получить экран"""
