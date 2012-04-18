2140098906af63f85f3516fbae57677dfe43d08c
Mikhail Fedosov <tbs.micle@gmail.com>
2012-04-19 01:00:46 +0400

    Drawing smoothly scaled image, woah!

--- app.py
+++ app.py
@@ -3,25 +3,34 @@ __author__ = "Mikhail Fedosov"
 
 import sys
 import PySide
-from PySide import QtGui
-from PySide.QtCore import *
+from PySide import QtGui, QtCore
 from PySide.QtGui import *
 
 class ScreenViewWindow(QDialog):
 	def __init__(self, parent=None):
 		super(ScreenViewWindow, self).__init__(parent)
 		self.setWindowTitle(u"Просмотр экрана")
+		self.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed))
 		self.initTrayIcon()
 
-		self.imgPreview = QtGui.QPixmap("trayico.png")
+		pix = QPixmap("trayico.png")
+		pix = pix.copy(0, 0, 16, 16).scaled(320, 240, QtCore.Qt.IgnoreAspectRatio, QtCore.Qt.SmoothTransformation)
+		self.imgPreview = QLabel()
+		self.imgPreview.setFixedSize(320, 240)
+		self.imgPreview.setPixmap(pix)
+		#self.imgPreview = QtGui.QWidget()
+		#self.imgPreview.setFixedSize(500, 500)
+		#self.imgPreview.setP
+		#image = QtGui.QImage("trayico.png")
+		#self.imgPreview.pa
 
-		self.btnPush = QtGui.QPushButton(u"Отправка картинки")
+		self.btnPush = QPushButton(u"Отправка картинки")
 		self.btnPush.clicked.connect(self.pushScreen)
-		self.btnPull = QtGui.QPushButton(u"Загрузка картинки")
+		self.btnPull = QPushButton(u"Загрузка картинки")
 		self.btnPull.clicked.connect(self.pullScreen)
 
 		layout = QVBoxLayout()
-		#layout.addWidget(self.imgPreview)
+		layout.addWidget(self.imgPreview)
 		layout.addWidget(self.btnPush)
 		layout.addWidget(self.btnPull)
 		self.setLayout(layout)
@@ -36,16 +45,16 @@ class ScreenViewWindow(QDialog):
 
 	def initTrayIcon(self):
 		"""Инициализация иконки в системном трее"""
-		self.trayIconIcon = PySide.QtGui.QIcon("trayico.png")
+		self.trayIconIcon = QIcon("trayico.png")
 
-		self.actionSendScreenshot = QtGui.QAction(u"Отправить экран", self, triggered=self.pushScreen)
-		self.actionQuit = QtGui.QAction(u"Выход", self, triggered=QtGui.qApp.quit)
+		self.actionSendScreenshot = QAction(u"Отправить экран", self, triggered=self.pushScreen)
+		self.actionQuit = QAction(u"Выход", self, triggered=QtGui.qApp.quit)
 
-		self.trayIconMenu = QtGui.QMenu(self)
+		self.trayIconMenu = QMenu(self)
 		self.trayIconMenu.addAction(self.actionSendScreenshot)
 		self.trayIconMenu.addAction(self.actionQuit)
 
-		self.trayIcon = PySide.QtGui.QSystemTrayIcon(self)
+		self.trayIcon = QSystemTrayIcon(self)
 		self.trayIcon.setIcon(self.trayIconIcon)
 		self.trayIcon.setContextMenu(self.trayIconMenu)
 		self.trayIcon.show()
