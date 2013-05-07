bc2395b55aa5eb164cf71549e6083a01d5d652c7
Mikhail Fedosov <tbs.micle@gmail.com>
2013-05-08 01:00:25 +0400

    receive `--tcp\udp-port` cli params

--- __init__.py
+++ __init__.py
@@ -3,9 +3,15 @@ __author__ = "Mikhail Fedosov <tbs.micle@gmail.com>"
 
 import uuid
 import logging
+import argparse
 
-APP_PORT = 7740
-APP_BROADCAST_PORT = 7741
+parser = argparse.ArgumentParser(description='QSH')
+parser.add_argument('--tcp-port', dest='tcp_port', default=7740, type=int, help='TCP port')
+parser.add_argument('--udp-port', dest='udp_port', default=7741, type=int, help='broadcast (UDP) port')
+args = parser.parse_args()
+
+APP_PORT = args.tcp_port
+APP_BROADCAST_PORT = args.udp_port
 APP_UUID = uuid.uuid4()
 APP_HELLO_MSG = "QSH_HELLO"
 APP_BYE_MSG = "QSH_BYE"
