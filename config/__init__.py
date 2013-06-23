be3c5688afbc0fa6f4d605b14f42819ab14fc700
Mikhail Fedosov <tbs.micle@gmail.com>
2013-06-23 23:06:51 +0400

    Config file should be in home dir for OS X app, fix #8 using `QDesktopServices.storageLocation(QDesktopServices.DataLocation)` path

--- config/__init__.py
+++ config/__init__.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
 __author__ = "Mikhail Fedosov <tbs.micle@gmail.com>"
 
+import os
 import uuid
 import shelve
 import logging
 import argparse
 
-parser = argparse.ArgumentParser(description="QSH")
-parser.add_argument("--tcp-port", dest="tcp_port", default=7740, type=int, help="TCP port")
-parser.add_argument("--udp-port", dest="udp_port", default=7741, type=int, help="broadcast (UDP) port")
-parser.add_argument("--config", dest="config", default="app_config", type=str, help="config file location")
-args = parser.parse_args()
-
-APP_PORT = args.tcp_port
-APP_BROADCAST_PORT = args.udp_port
-APP_UUID = uuid.uuid4()
-APP_HELLO_MSG = "QSH_HELLO"
-APP_BYE_MSG = "QSH_BYE"
+from PySide.QtGui import QDesktopServices
 
 
 class AppConfig:
+
+	@classmethod
+	def get_config_path(cls):
+		config_path = os.path.join(QDesktopServices.storageLocation(QDesktopServices.DataLocation), "qsh")
+		if not os.path.exists(config_path):
+			os.makedirs(config_path)
+		return os.path.join(config_path, "config")
 
 	@classmethod
 	def get_username(cls):
@@ -34,6 +32,18 @@ class AppConfig:
 		d["app.username"] = value
 		d.close()
 
+
+parser = argparse.ArgumentParser(description="QSH")
+parser.add_argument("--tcp-port", dest="tcp_port", default=7740, type=int, help="TCP port")
+parser.add_argument("--udp-port", dest="udp_port", default=7741, type=int, help="broadcast (UDP) port")
+parser.add_argument("--config", dest="config", default=AppConfig.get_config_path(), type=str, help="config file location")
+args = parser.parse_args()
+
+APP_PORT = args.tcp_port
+APP_BROADCAST_PORT = args.udp_port
+APP_UUID = uuid.uuid4()
+APP_HELLO_MSG = "QSH_HELLO"
+APP_BYE_MSG = "QSH_BYE"
 
 SCREEN_IMAGE_TYPE = "PNG"
 SCREEN_IMAGE_QUALITY = 40
